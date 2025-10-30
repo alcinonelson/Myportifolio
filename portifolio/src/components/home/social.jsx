2989a6b9c7b6a559a19047f7c3325bb5eb23bf2c
Alcino <alcino.maido99@gmail.com>
2025-10-30 02:57:40 +0200

    feat: update social media links with actual profile URLs
    Updated social media links in home section:
    - GitHub: https://github.com/alcinonelson
    - LinkedIn: https://www.linkedin.com/in/alcinonelson/
    - Instagram: https://www.instagram.com/debugger69_/

--- portifolio/src/components/home/social.jsx
+++ portifolio/src/components/home/social.jsx
@@ -1,13 +1,13 @@
 const Social = () => {
     return (
         <div className="home__social">
-            <a href="https://github.com" className="home__social-icon" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/alcinonelson" className="home__social-icon" target="_blank" rel="noopener noreferrer">
                 <i className="uil uil-github-alt"></i>
             </a>
-            <a href="https://linkedin.com" className="home__social-icon" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/alcinonelson/" className="home__social-icon" target="_blank" rel="noopener noreferrer">
                 <i className="uil uil-linkedin-alt"></i>
             </a>
-            <a href="https://instagram.com" className="home__social-icon" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/debugger69_/" className="home__social-icon" target="_blank" rel="noopener noreferrer">
                 <i className="uil uil-instagram"></i>
             </a>
         </div>
